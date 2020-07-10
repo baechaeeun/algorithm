a5ff28fb63b626a3ac572655e879ee12b7de672f
baechaeeun <bchaeeun@naver.com>
2020-07-10 22:39:26 +0900

    Add files via upload

--- algorithm_6603.java
+++ algorithm_6603.java
@@ -1,44 +1,43 @@
 package algorithm_dfs;
 import java.util.*;
 public class algorithm_6603 {
-	static int k;
-	static int [] arr;
-	static boolean[] skip;
-	
-	static void dfs(int line,int count) {
-		if(count==6) {
-			for(int i=0;i<k;i++)
-				if(skip[i]=true)
-					System.out.print(arr[i]+" ");
-			System.out.println("");
-		}
-		
-		for(int j=line;j<k;j++) {
-			skip[j]=true;
-			dfs(j+1,count+1);
-			skip[j]=false;
-		}
-	}
-	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-		
-		while(true) {
-			k=sc.nextInt();
-			if(k==0)
-				break;
-			
-			arr=new int[k];
-			skip=new boolean[k];
-			
-			for(int i=0;i<arr.length;i++)
-				arr[i]=sc.nextInt();
-			
-			Arrays.sort(arr);
-			
-			dfs(0,0);
-			System.out.println();
-		}
-		sc.close();
-	}
+	static int N;
+    static int[] group;
+    static ArrayList<Integer> numbers;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            N = sc.nextInt();
+            if (N == 0)
+                break;
+            group = new int[N];
+
+            for (int i = 0; i < N; i++)
+                group[i] = sc.nextInt();
+
+            Arrays.sort(group);
+
+            numbers = new ArrayList<>();
+
+            combination(0, 0);
+
+            System.out.println();
+        }
+    }
+
+    static void combination(int depth, int idx) {
+        if (depth == 6) {
+        	for (int i = 0; i < 6; i++) 
+                System.out.print(numbers.get(i) + " ");
+            System.out.println();
+        }
+
+        for (int i = idx; i < group.length; i++) {
+            numbers.add(group[i]);
+            combination(depth + 1, i + 1);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
 }
